5c026c2a8b6f8ea5079858414ee189f060c0d571
GuiRay <guiray@users.noreply.github.com>
2022-06-09 00:36:29 -0400

    launcher can launch coins

---
+++ Assets/Scripts/LauncherMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherMovement : MonoBehaviour
+{
+    //public float rotateSpeed = 180f;
+
+    private
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
